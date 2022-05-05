8c5a00fd785e9f032142f3751643af4d7894291f
Trenton Lewis <lewitren@flip2.engr.oregonstate.edu>
2022-05-05 00:13:38 -0700

    95% of the way there, needs looked over

--- DMQ.sql
+++ DMQ.sql
@@ -4,15 +4,16 @@
 -- Project Name: Beavit
 
 -- Queries needed per Project Outline:
-    -- Insert entries to every table/entity (5 entities for us so 5 of these)
-    -- Every Table/entity should be used in at least one SELECT (5 entities for us so 5 of these)
+    -- Insert entries to every table/entity (5 entities for us so 5 of these) √
+    -- Every Table/entity should be used in at least one SELECT (5 entities for us so 5 of these) √
         -- Needs to be search/filterable
-    -- One DELETE for any entity for M:M
-    -- One UPDATE for any entity for M:M
+    -- One DELETE for any entity for M:M √
+    -- One UPDATE for any entity for M:M √
     -- In a 1:M relationship, you should be able to set FK to NULL and remove the relationship
 ---------------------------------------------------------------------------------------------------------------------------------------------
 
 -- *** $ symbol means whatever the user of the UI inputs ***
+
 ---------------------------------------------------------------------------------------------------------------------------------------------
 -- Users Queries:
 
@@ -22,6 +23,17 @@ VALUES ($Username, $JoinDate, ThumbsUpCt, $ThumbsDwnCt);
     
     -- Display all users
 SELECT Username, JoinDate AS "Join Date", ThumbsUpCt AS "Thumbs Up Count", ThumbsDwnCt AS "Thumbs Down Count" FROM Users;
+
+    -- Update a User (Two Parts)
+        -- Get specific user's info and display it
+SELECT * FROM Users WHERE Username = $UsersInputHere;
+        -- Update that specific user's info
+UPDATE Users SET Username = "$UsernameInput", JoinDate = "$JoinDateInput", 
+    ThumbsUpCt = "$ThumbsUpInput", ThumbsDwnCt = "ThumbsDwnInput"
+WHERE UserID = "$UserIDFromForm";
+
+    -- Delete a User
+DELETE FROM Users WHERE UserID = $UserSelectionFromList;
 
 ---------------------------------------------------------------------------------------------------------------------------------------------
 -- Posts Queries:
@@ -37,6 +49,18 @@ SELECT Users.Username AS "Poster", Posts.PostTitle AS "Post Title",
 FROM Posts
 JOIN Users ON Posts.OP_UserID = Users.UserID
 JOIN Communities ON Posts.Communities_CommunityID = Communities.CommunityID;
+    
+    -- Update a Post (Two Parts)
+        -- Get specific post's info and display it
+SELECT * FROM Posts WHERE PostID = $UserSelectionFromDropDownThatProvidesListOfPostIDs;
+        -- Update that specific post's info
+UPDATE Posts SET OP_UserID = "$UserIDInput", PostTitle = "$PostTitleInput",
+    ThumbsUpCt = "$ThumbsUpInput", ThumbsDwnCt = "ThumbsDwnInput",
+    DatePosted = "$DatetimeInput", Communities_CommunityID = "CommunityIDInput"
+WHERE PostID = $UserSelectionFromDropDownThatProvidesListOfPostIDs;
+
+    -- Delete a Post
+DELETE FROM Posts WHERE PostID = $UserSelectionFromListThatProvidesUsernameAndPostTitle;
 
 ---------------------------------------------------------------------------------------------------------------------------------------------
 -- Communities Queries:
@@ -47,6 +71,16 @@ VALUES ($CommunityName, $MemberCt);
 
     -- Display all communities
 SELECT CommunityName, MemberCt FROM Communities;
+
+    -- Update a Community (Two Parts)
+        -- Get specific post's info and display it
+SELECT * FROM Communities WHERE CommunityName = $UserSelection;
+        -- Update that specific community's info
+UPDATE Communities SET CommunityName = "$UsersChoice", MemberCt = "$UsersNumber"
+WHERE CommunityName = $UserSelection;
+
+    -- Delete a Community
+DELETE FROM  Communities WHERE CommunityName = $UserSelectionFromList;
 
 ---------------------------------------------------------------------------------------------------------------------------------------------
 -- Comments Queries:
@@ -63,6 +97,19 @@ FROM Comments
 JOIN Users ON Comments.Commenter_UserID = Users.UserID
 JOIN Posts ON Comments.Parent_Post_PostID = Posts.PostID;
 
+    -- Update a Comment (Two Parts)
+        -- Get specific comment's info and display it
+SELECT * FROM Comments WHERE CommentID = $UserSelectionFromDropDownThatProvidesListOfCommentIDs;
+        -- Update that specific comment's info
+UPDATE Comments SET ThumbsUpCt = "$ThumbsUpInput", ThumbsDwnCt = "ThumbsDwnInput",
+    DateMade = "$DatetimeInput", CommentStr = "$CommentStrInput",
+    Commenter_UserID = "$UserIDInput", Parent_Post_PostID = "$UsersParentPostInput",
+    Parent_Comment_CommentID = "$UsersParentCommentIDInput"
+WHERE CommentID = $UserSelectionFromDropDownThatProvidesListOfCommentIDs;
+
+   -- Delete a Comment
+DELETE FROM Comments WHERE CommentID = $UserSelectionThatProvidesCommenterUsernameAndCommentStr;
+
 ---------------------------------------------------------------------------------------------------------------------------------------------
 -- Community_User Queries
     -- Create (Insert) New 
@@ -71,7 +118,7 @@ VALUES (
     (SELECT UserID FROM Users WHERE Username="$UserInputHere"), 
     (SELECT CommunityID FROM Communities WHERE CommunityName = "$UserInputHere"),
     $TrueOrFalseUserInput
-)
+);
     -- Display each user and show what communities they belong to as well as if they moderate that community
 
 SELECT Users.Username AS "User", Communities.CommunityName AS "Community", Community_User_Base.ModeratorStatus AS "Moderator Status"
@@ -79,3 +126,9 @@ FROM Community_User_Base
 JOIN Users ON Community_User_Base.Users_UserID = Users.UserID
 JOIN Communities ON Communities.CommunityID = Community_User_Base.Communities_CommunityID
 ORDER BY Users.Username;
+
+    -- Update community/user relationship
+UPDATE Community_User_Base SET Users_UserID = "$UserIDOfUserSelectionFromDropdownOfUsernames", Communities_CommunityID = "$CommunityIDOfUserSelectionFromDropdownOfCommunityNames", ModeratorStatus = $TrueOrFalseUserInput
+
+    -- Delete community/user relationship if the user unsubscribes
+DELETE FROM Community_User_Base WHERE Users_UserID = "$UserIDOfUserSelectionFromDropdownOfUsernames" AND Communities_CommunityID = "$CommunityIDOfUserSelectionFromDropdownOfCommunityNames";
